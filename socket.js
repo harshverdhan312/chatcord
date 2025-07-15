3b9828a5f7966115279e739fbdac354d4a923ea8
Harsh Verdhan Singh <harshverdhansingh1@gmail.com>
2025-07-15 11:01:15 +0530

    removed redis

--- socket.js
+++ socket.js
@@ -1,10 +1,8 @@
 const { Server } = require("socket.io");
 const chatController = require("./controllers/chatController");
-const { setupRedisAdapter } = require("./utils/redisClient");
 
 async function setupSocket(server) {
   const io = new Server(server);
-  await setupRedisAdapter(io);
   chatController(io);
 }
 
